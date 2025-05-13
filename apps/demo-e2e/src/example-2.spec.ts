d4f0b45dfd7ce8ed3e1b9896ce74153c3d3c026b
Jack Hsu <jack.hsu@gmail.com>
2025-05-13 15:34:16 -0400

    add more tests

---
+++ apps/demo-e2e/src/example-2.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  await page.waitForTimeout(30_000);
+
+  // Expect h1 to contain a substring.
+  expect(await page.locator('h1').innerText()).toContain('Welcome');
+});
+
+test('has title 2', async ({ page }) => {
+  await page.goto('/');
+
+  await page.waitForTimeout(30_000);
+
+  // Expect h1 to contain a substring.
+  expect(await page.locator('h1').innerText()).toContain('Welcome');
+});
+
+test('has title 3', async ({ page }) => {
+  await page.goto('/');
+
+  await page.waitForTimeout(30_000);
+
+  // Expect h1 to contain a substring.
+  expect(await page.locator('h1').innerText()).toContain('Welcome');
+});
